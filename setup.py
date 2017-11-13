3ed921fc072ba3c3a8e4a956c223ebb382b8d4a4
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2017-11-13 12:24:29 +0100

    Add setup.py and a simple existing script to find the total time.

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(name='nvprof',
+      version='0.1',
+      description='NVIDIA Profier tools',
+      url='https://github.com/rossumai/nvprof',
+      author='Bohumir Zamecnik',
+      author_email='bohumir.zamecnik@gmail.com',
+      license='MIT',
+      packages=['nvprof'],
+      zip_safe=False,
+      install_requires=[
+         'sqlite',
+         'numpy',
+      ],
+      setup_requires=['setuptools-markdown'],
+      long_description_markdown_filename='README.md',
+      # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+
+          'License :: OSI Approved :: MIT License',
+
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+
+          'Operating System :: POSIX :: Linux',
+      ])
