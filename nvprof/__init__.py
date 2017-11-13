55965745ab3bb82ec0602b3fc030f78e199a8dde
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2017-11-13 13:45:29 +0100

    Add methods to delete unnecessary tables and list event counts.

--- nvprof/__init__.py
+++ nvprof/__init__.py
@@ -1,13 +1,10 @@
 # NVIDIA Profier (nvprof) utilities
-#
-# - figure total execution time (so that we can sample a tiny bit)
-#
-# TODO:
-# - remove data unnecessary for further analysis (reducing the dataset size)
 
 import sys
 
 import sqlite3
+
+table_prefix = 'CUPTI_ACTIVITY_KIND_'
 
 def total_time(conn, table='CUPTI_ACTIVITY_KIND_MEMCPY'):
     c = conn.cursor()
@@ -19,6 +16,33 @@ def list_tables(conn):
     c = conn.cursor()
     c.execute('select name from sqlite_master where type="table" and name like "CUPTI%" order by name')
     return [row[0] for row in c.fetchall()]
+
+def table_sizes(conn):
+    table_sizes = {}
+    c = conn.cursor()
+    for table in list_tables(conn):
+        c.execute('select count(*) from %s' % table)
+        table_sizes[table] = c.fetchone()[0]
+    return table_sizes
+
+def total_event_count(conn):
+    ts = table_sizes(conn)
+    return sum(ts.values())
+
+def biggest_tables(conn):
+    ts = table_sizes(conn)
+    return sorted([(s, n) for (n, s) in ts.items() if s > 0], reverse=True)
+
+def truncate_tables(conn, tables):
+    c = conn.cursor()
+    for table in ['RUNTIME', 'DRIVER']:
+        c.execute('DELETE FROM %s%s' % table_prefix)
+    c.execute('VACUUM')
+    conn.commit()
+
+def delete_unnecessary_events(conn):
+    tables = ['%s%s' % (table, table_prefix) for table in ['RUNTIME', 'DRIVER']]
+    truncate_tables(conn, tables)
 
 if __name__ == '__main__':
     db_name = sys.argv[1]
